6295f163c2c33fbed00c9e1fe6fd3d7c0bfd3c11
koen505 <koen.verhaegh@gmail.com>
2016-03-10 15:09:56 +0100

    added GeoCoding Parser en Open Weather
    permissie voor internet ook toegevoegd

---
+++ app/src/main/java/tue/easyevents/GeoCodingParser.java
@@ -0,0 +1,82 @@
+package tue.easyevents;
+
+import org.apache.http.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import android.util.Log;
+
+public class GeoCodingParser {
+
+    static InputStream is = null;
+    static JSONObject json = null;
+    static String outPut = "";
+    public static String url =  "https://maps.googleapis.com/maps/api/geocode/JSON?address=";
+    public static String address = "";
+    public static String key = "+CA&Key=AIzaSyANaZSYC__XBBCPvkP8VGbdfW7OX86w9PQ";
+    public static String call = url + address + key;
+    public String location = "";
+
+    // constructor
+    public GeoCodingParser() {
+
+    }
+
+    public JSONObject getJSONFromUrl(String url, List<NameValuePair> params) {
+
+        // Making the HTTP request
+        try {
+
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            is = httpEntity.getContent();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            outPut = sb.toString();
+            Log.e("JSON", outPut);
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        try {
+            json = new JSONObject(outPut);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return json;
+
+    }
+}
