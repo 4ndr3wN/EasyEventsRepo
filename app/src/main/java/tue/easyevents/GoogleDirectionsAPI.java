425135bbfd79ad970b5c515b33836c5bd5cbc546
Andrew N <andrewnoorden@hotmail.com>
2016-03-14 11:38:59 +0100

    Added splashscreen changed AndroidManifest.xml
    Added splashscreen changed AndroidManifest.xml, commented
    googledirectionsapi, added easyevents png and preloader layout

--- app/src/main/java/tue/easyevents/GoogleDirectionsAPI.java
+++ app/src/main/java/tue/easyevents/GoogleDirectionsAPI.java
@@ -1,20 +1,6 @@
 package tue.easyevents;
 
-import android.util.Xml;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Array;
 import java.net.ConnectException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 
 /**
  * Created by Boy on 11-3-2016.
@@ -34,14 +20,14 @@ public class GoogleDirectionsAPI {
      * @return An arraylist of directions fitting the criteria and parameters
      * @throws ConnectException
      */
-    public static ArrayList<WAAROMWERKTDEZEVARIABELENIET> searchEvents(String origin, String destination, String mode, int departure_time, ArrayList<String> cat)
-            throws ConnectException {
-
-        InputStream in = null;
-        ArrayList<WAAROMWERKTDEZEVARIABELENIET> routes = new ArrayList<>();
-        String SEARCH_PARAMETERS = "origin=" + origin + "&destination=" + destination + "&mode=" + mode + "&departure_time=" + departure_time;
-        String SEARCH_ADDRESS = BASE_ADDRESS + SEARCH_PARAMETERS + API_KEY;
-    }
+//    public static ArrayList<WAAROMWERKTDEZEVARIABELENIET> searchEvents(String origin, String destination, String mode, int departure_time, ArrayList<String> cat)
+//            throws ConnectException {
+//
+//        InputStream in = null;
+//        ArrayList<WAAROMWERKTDEZEVARIABELENIET> routes = new ArrayList<>();
+//        String SEARCH_PARAMETERS = "origin=" + origin + "&destination=" + destination + "&mode=" + mode + "&departure_time=" + departure_time;
+//        String SEARCH_ADDRESS = BASE_ADDRESS + SEARCH_PARAMETERS + API_KEY;
+//    }
 }
 
 
