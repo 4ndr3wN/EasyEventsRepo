bd3d68dc0b98e126396ce913e77a9e8008156e21
Andrew N <andrewnoorden@hotmail.com>
2016-03-15 11:58:50 +0100

    Updated settings Activity layout
    Updated settings Activity layout

--- app/src/main/java/tue/easyevents/Settings_Activity.java
+++ app/src/main/java/tue/easyevents/Settings_Activity.java
@@ -2,6 +2,7 @@ package tue.easyevents;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class Settings_Activity extends AppCompatActivity {
 
@@ -9,13 +10,15 @@ public class Settings_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 //
 //
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-//        getSupportActionBar().setDisplayShowTitleEnabled(false);
-//
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+//        getSupportActionBar().setDisplayOptions();
+////
     }
 
 }
