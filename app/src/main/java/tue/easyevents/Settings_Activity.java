02698ee5984c0a424ec0e19d6e08bd118f96d402
pimovietc <pimovietc@gmail.com>
2016-03-19 15:39:48 +0100

    Finalized Settings View
    - Added the actual UI-views to the Settings View
    - Quick Reference for Back-End devs in the settings_view.xml

--- app/src/main/java/tue/easyevents/Settings_Activity.java
+++ app/src/main/java/tue/easyevents/Settings_Activity.java
@@ -3,6 +3,8 @@ package tue.easyevents;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Settings_Activity extends AppCompatActivity {
 
@@ -19,6 +21,21 @@ public class Settings_Activity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 //        getSupportActionBar().setDisplayOptions();
 ////
+
+        // Code for dropdowns, see settings_view.xml for instructions on how to use
+        // or check: http://developer.android.com/guide/topics/ui/controls/spinner.html
+        Spinner spinner_time = (Spinner) findViewById(R.id.spinner_time);
+        ArrayAdapter<CharSequence> adapter_time = ArrayAdapter.createFromResource(this,
+                R.array.time_array, android.R.layout.simple_spinner_item);
+        adapter_time.setDropDownViewResource(R.layout.dropdown_layouts);
+        spinner_time.setAdapter(adapter_time);
+
+        Spinner spinner_range = (Spinner) findViewById(R.id.spinner_range);
+        ArrayAdapter<CharSequence> adapter_range = ArrayAdapter.createFromResource(this,
+                R.array.range_array, android.R.layout.simple_spinner_item);
+        adapter_range.setDropDownViewResource(R.layout.dropdown_layouts);
+        spinner_range.setAdapter(adapter_range);
+
     }
 
 }
