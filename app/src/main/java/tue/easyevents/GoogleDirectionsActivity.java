1c11e077764655397e936788a501ee6136ac0b36
koen505 <koen.verhaegh@gmail.com>
2016-03-25 17:11:45 +0100

    Transport standard location/user location check + use those coords and event coords
    see title

--- app/src/main/java/tue/easyevents/GoogleDirectionsActivity.java
+++ app/src/main/java/tue/easyevents/GoogleDirectionsActivity.java
@@ -2,10 +2,15 @@ package tue.easyevents;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.Button;
 
@@ -25,18 +30,25 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
 public class GoogleDirectionsActivity extends AppCompatActivity implements OnMapReadyCallback, DirectionCallback {
     private GoogleMap googleMap;
     private String serverKey = "AIzaSyDc-zOLX34KkiohWYfd0JOmlfbEGv2VT9M";
     //change preferably according to center of origin and destination
-    private LatLng camera = new LatLng(51.4, 5.79);
+    private LatLng camera;
     //change according to origin for directions
     private LatLng origin;
     //change according to destination (location of event)
-    private LatLng destination = new LatLng(51.4, 5.79);
+    private LatLng destination;
 
     String latitude;
     String longitude;
@@ -46,8 +58,13 @@ public class GoogleDirectionsActivity extends AppCompatActivity implements OnMap
     public String eventLoc;
     public String latitudeLoc;
     public String longitudeLoc;
-    double eventLa;
-    double eventLo;
+    public double eventLa;
+    public double eventLo;
+    public String standardLoc;
+    public String userLoc;
+    public String geoCodedOrigin;
+    public double cameraLat;
+    public double cameraLon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,27 +75,52 @@ public class GoogleDirectionsActivity extends AppCompatActivity implements OnMap
         longitude = intent.getStringExtra("lon");
         eventLoc = intent.getStringExtra("eventLoc");
 
-        //StringTokenizer st = new StringTokenizer(eventLoc, ",");
-        //latitudeLoc = st.nextElement().toString();
-        //longitudeLoc = st.nextElement().toString();
+        //check settings for standard origin (user location or different address)
+        //see if the "use location" checkbox is checked
+        //if so, set la and lo to the users latitude and longitude
+        userLoc = inputFile("GPSCheck");
+        if (userLoc.equals("1")) {
+            //Parse user location strings to double
+            la = Double.parseDouble(latitude);
+            lo = Double.parseDouble(longitude);
+        } else {
+            //use location checkbox not set
+            //use standard location
+            standardLoc = inputFile("address_file");
+            new GeoCode().execute();
 
-       // eventLa = Double.parseDouble(latitudeLoc);
-       // eventLo = Double.parseDouble(longitudeLoc);
+            //split up geocoded location
+            StringTokenizer str = new StringTokenizer(geoCodedOrigin, ",");
+            String oriLat = str.nextElement().toString();
+            String oriLon = str.nextElement().toString();
 
-        //Parse strings to double
-        la = Double.parseDouble(latitude);
-        lo = Double.parseDouble(longitude);
-
-        //set origin LatLng
+            //parse oriLat and oriLon to double
+            la = Double.parseDouble(oriLat);
+            lo = Double.parseDouble(oriLon);
+        }
+        //set origin LatLng according to above if-else clause
         origin = new LatLng(la, lo);
 
+        //split up event location
+        StringTokenizer st = new StringTokenizer(eventLoc, ",");
+        latitudeLoc = st.nextElement().toString();
+        longitudeLoc = st.nextElement().toString();
+
+        eventLa = Double.parseDouble(latitudeLoc);
+        eventLo = Double.parseDouble(longitudeLoc);
+
         //set destination LatLng
-        //destination = new LatLng(eventLa, eventLo);
+        destination = new LatLng(eventLa, eventLo);
 
-        //TODO: fix origin
-        //TODO: Set destination according to event location
-        //TODO: Check settings for the checkbox/address
+        //set camera latitude in the middle of the origin and destination
+        cameraLat = (eventLa + la)/2;
+        //set camera longitude in the middle of the origin and destination
+        cameraLon = (eventLo + lo)/2;
 
+        //set camera
+        camera = new LatLng(cameraLat, cameraLon);
+
+        //TODO: find some way to set the zoom appropriately depending on the distance
 
         setContentView(R.layout.directions_activity);
 
@@ -88,6 +130,7 @@ public class GoogleDirectionsActivity extends AppCompatActivity implements OnMap
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
+        //TODO: find some way to set the zoom appropriately depending on the distance
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(camera, 9));
 
         requestDirectionDriving();
@@ -140,4 +183,42 @@ public class GoogleDirectionsActivity extends AppCompatActivity implements OnMap
         //nog niks
         Log.d("KAPOT", "WAAROM WERKT HET NIET");
     }
+
+    public String inputFile(String file_name) {
+        String saved = "";
+        String read;
+        //check for fileinput files
+        try {
+            FileInputStream fis = openFileInput(file_name);
+            StringBuffer buffer = new StringBuffer();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+            if (fis != null) {
+                while ((read = reader.readLine()) != null) {
+                    buffer.append(read);
+                }
+            }
+            fis.close();
+            saved = buffer.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return saved;
+    }
+
+    //use an Asynchronous Task to do the GeoCodingAPI and EventfulAPI calls
+    public class GeoCode extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                geoCodedOrigin = GeoCodingAPI.geoCode(standardLoc);
+            } catch (ConnectException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
 }
